141eb8e27a3dc293fa0d0c80753855598438c445
tangyangyang <tangyang22007@126.com>
2022-05-20 14:23:30 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,12 +1,12 @@
 DB_USER = 'root'
-DB_PASSWORD = ''
-DB_HOST = 'localhost'
-DB_DB = 'flask-pyjwt-auth'
+DB_PASSWORD = '~Qscvgy234!!'
+DB_HOST = '139.155.76.178'
+DB_DB = 'data'
 
 DEBUG = True
 PORT = 3333
-HOST = "192.168.1.141"
-SECRET_KEY = "my blog"
+HOST = "139.155.76.178"
+SECRET_KEY = "test key --modify me"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = 'mysql://' + DB_USER + ':' + DB_PASSWORD + '@' + DB_HOST + '/' + DB_DB
