fb26c9ebbbe9ad06c17af5846d74d8c19e89aad4
tangyangyang <tangyang22007@126.com>
2022-05-20 14:23:12 +0800

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,4 @@ from app import create_app
 app = create_app('app.config')
 
 if __name__ == '__main__':
-    app.run(host=app.config['HOST'],
-            port=app.config['PORT'],
-            debug=app.config['DEBUG'])
-
+    app.run(host="127.0.0.1", port=5000, debug=True)
