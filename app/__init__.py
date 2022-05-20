9960b80b41be58cc01f36a8d1651badf3c2169fd
tangyangyang <tangyang22007@126.com>
2022-05-20 14:24:13 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
+from ast import Str
+from unittest.mock import patch
+from flask import Flask, request, jsonify, g
 from flask_sqlalchemy import SQLAlchemy
-
+from . import common
 db = SQLAlchemy()
 
 def create_app(config_filename):
@@ -16,6 +18,23 @@ def create_app(config_filename):
             if headers:
                 response.headers['Access-Control-Allow-Headers'] = headers
         return response
+
+    @app.before_request
+    def before_request():
+        if (request.path == '/login' or request.path == '/register'):
+            return None
+        else:
+            auth_header = request.headers.get('Authorization')
+            if (not auth_header):
+                return jsonify(common.falseReturn(msg="未登录", code=401))
+            else:
+                payload = common.decode_auth_token(auth_header)
+                # return payload
+                if (isinstance(payload, str)): 
+                    return jsonify(common.falseReturn(msg=payload, code=401))
+                g.user_id = payload.id
+                g.login_time = payload.login_time
+            return None
 
     from app.users.model import db
     db.init_app(app)
